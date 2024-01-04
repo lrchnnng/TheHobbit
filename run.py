3e3680aae98406671d818377a9e62c3e26b8bd54
Laura Channing <laurachanning@hotmail.co.uk>
2024-01-04 14:18:19 +0000

    added bootstrap files

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
+import os
 from flask import Flask, render_template
-
 
 app = Flask(__name__)
 
@@ -12,8 +12,16 @@ def index():
 def about():
     return render_template('about.html')
 
+@app.route("/contact")
+def contact():
+    return render_template('contact.html')
+
+@app.route("/careers")
+def careers():
+    return render_template("careers.html")
+
 if __name__ == "__main__":
     app.run(
-        host="0.0.0.0", 
-        port=5000, 
+        host=os.environ.get("IP", "0.0.0.0"), 
+        port=int(os.environ.get("PORT", "5000")), 
         debug=True)
